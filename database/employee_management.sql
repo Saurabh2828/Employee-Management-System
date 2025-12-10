1866639d1746bc6e92a6a0d1716e04053ff3e7f4
Saurabh2828 <saurabhlavhe2812@gmail.com>
2025-12-10 17:59:29 +0530

    Update employee_management.sql

--- database/employee_management.sql
+++ database/employee_management.sql
@@ -1,11 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 03, 2021 at 02:30 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
